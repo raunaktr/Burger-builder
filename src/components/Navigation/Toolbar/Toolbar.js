3222205116220a7b66aa2f0f5795cdd2bf05ee64
Raunak <reachtoraunak@gmail.com>
2020-07-20 23:27:44 +0530

    Reusable nav and nav items

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,12 +2,15 @@ import React from "react";
 import classes from "./Toolbar.css";
 
 import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
 
 const toolbar = () => (
   <header className={classes.Toolbar}>
     <div>Menu</div>
     <Logo />
-    <nav>...</nav>
+    <nav>
+      <NavigationItems />
+    </nav>
   </header>
 );
 
