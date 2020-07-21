1d70de19a84f8851eaff7037fa527fe0ab783bee
Raunak <reachtoraunak@gmail.com>
2020-07-21 16:24:46 +0530

    Adding ToggleSideDrawer Component and Layout into Class based

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,14 +3,15 @@ import classes from "./Toolbar.css";
 
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
 
-const toolbar = () => (
+const toolbar = (props) => (
   <header className={classes.Toolbar}>
-    <div>Menu</div>
+    <DrawerToggle clicked={props.drawerToggleClicked} />
     <div className={classes.Logo}>
       <Logo />
     </div>
-    <nav className={classes.DesktopOnly} >
+    <nav className={classes.DesktopOnly}>
       <NavigationItems />
     </nav>
   </header>
